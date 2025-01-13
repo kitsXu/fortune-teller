61f6a9149acd87eeadb54737ed4f7a676f655a1f
Solah <solah.kyle.skye@gmail.com>
2025-01-12 22:33:04 -0600

    basic hide/show function for image switch, art added

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,36 @@
-import { useState } from 'react';
+import { useState} from 'react';
 import fortuneTellerEven from './images/fortuneEven.png/';
+import fortuneOdd from './images/fortuneOdd.png/';
 import './App.css'
 
 function App() {
+  const [evenVisibility, setEvenVisibility] = useState(true);
+  const [oddVisibility, setOddVisibility] = useState(false);
+
+  function showHide () {
+    if (evenVisibility === true)
+    setEvenVisibility(false)
+  else setEvenVisibility(true);
+  if (oddVisibility === false)
+    setOddVisibility(true);
+  else setOddVisibility(false);
+    console.log("SHOW-HIDE")
+  }
 
   return (
     <>
-    <div className='topSquareWrapper'>
+    {/* <div className='topSquareWrapper'>
       <div className='squareOne'></div>
       <div className='squareTwo'></div>
       </div>
       <div className='bottomSquareWrapper'>
       <div className='squareThree'></div>
       <div className='squareFour'></div>
-      </div>
-      <div>
-        <img className='fortuneImage' src={fortuneTellerEven}></img>
+      </div> */}
+      <div onClick={showHide}>
+        {evenVisibility && <img className='fortuneImageEven' src={fortuneTellerEven}></img>}
+        {oddVisibility && <img className='fortuneImageOdd' src={fortuneOdd}></img>}
+
       </div>
              </>
   )
