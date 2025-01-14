1fbf422a0b0beecd9c9c1ac2b2f218928fc46a62
Solah <solah.kyle.skye@gmail.com>
2025-01-13 20:54:35 -0600

    made a component for each stage of fortune teller

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,17 @@
-import { useState} from 'react';
-import fortuneTellerEven from './images/fortuneEven.png/';
-import fortuneOdd from './images/fortuneOdd.png/';
-import './App.css'
+import FortuneEven from "./components/FortuneEven.jsx";
+import FortuneOdd from "./components/fortuneOdd.jsx";
+import FortuneColors from "./components/fortuneColors.jsx";
+
+import "./App.css";
 
 function App() {
-  const [evenVisibility, setEvenVisibility] = useState(true);
-  const [oddVisibility, setOddVisibility] = useState(false);
 
-  function showHide () {
-    if (evenVisibility === true)
-    setEvenVisibility(false)
-  else setEvenVisibility(true);
-  if (oddVisibility === false)
-    setOddVisibility(true);
-  else setOddVisibility(false);
-    console.log("SHOW-HIDE")
-  }
 
   return (
     <>
-    {/* <div className='topSquareWrapper'>
-      <div className='squareOne'></div>
-      <div className='squareTwo'></div>
-      </div>
-      <div className='bottomSquareWrapper'>
-      <div className='squareThree'></div>
-      <div className='squareFour'></div>
-      </div> */}
-      <div onClick={showHide}>
-        {evenVisibility && <img className='fortuneImageEven' src={fortuneTellerEven}></img>}
-        {oddVisibility && <img className='fortuneImageOdd' src={fortuneOdd}></img>}
-
-      </div>
-             </>
-  )
+    <FortuneEven />
+    </>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/FortuneEven.jsx
@@ -0,0 +1,13 @@
+import fortuneTellerEven from "../images/fortuneEven.png";
+
+export default function FortuneEven() {
+
+  return (
+    <>
+<div>
+    <img className="fortuneImageEven" src={fortuneTellerEven}></img>
+</div>
+    </>
+  );
+}
+
