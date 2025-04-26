1f130fbee8a175d9f698ec2fbf601fc1c246f356
Solah <kitsxu.apps@gmail.com>
2025-04-26 12:59:49 -0500

    Update Button.jsx

--- src/Button.jsx
+++ src/Button.jsx
@@ -1,6 +1,15 @@
-
+import { useEffect } from "react"
 
 export default function Button() {
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      alert('were you looking for something dirty? Maybe in your email later ;)')
+    }, 2000)
+
+    return () => clearTimeout(timer)
+  }, [])
+
   return (
     <> 
   <div className='title'>Holy Pokes!!!!</div>
