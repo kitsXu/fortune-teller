a2c672515f0427a2ab4f5d517d0146a5e16da0d7
Solah <kitsxu.apps@gmail.com>
2025-04-26 12:51:17 -0500

    changed app

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,49 @@
-import FortuneEven from "./components/FortuneEven.jsx";
-import FortuneOdd from "./components/FortuneOdd.jsx";
-import FortuneColors from "./components/FortuneColors.jsx";
+import './App.css'
+import { useState } from 'react'
 
-import "./App.css";
-import { useState } from "react";
+import lostPokers from "./images/lostPokers.jpeg"
+import masterPoker from "./images/masterPoker.jpeg"
+import pokeMuppets from "./images/pokeMuppets.jpeg"
+import pokeMuppetsTwo from "./images/pokeMuppetsTwo.jpeg"
+import pokeMuscle from "./images/pokeMuscle.jpeg"
+import pokeOneH from "./images/pokeOneH.jpeg"
+import pokeOneT from "./images/pokeOneT.jpg"
+import pokeTwoTt from "./images/pokeTwoTt.jpeg"
+import pokeTwoT from "./images/pokeTwoT.png"
+import Button from './Button'
+
+
+
 
 function App() {
-  const [fortuneState, setFortuneState] = useState("colors");
+  const [view, setView] = useState("");
+  const [pageVisibility, setPageVisibility] = useState(true)
 
-  function red() {
-    event.preventDefault();
-    setFortuneState("even");
-    setTimeout(() => { setFortuneState("colors");}, 500);
-    setTimeout(() => { setFortuneState("odd");}, 700);
-    setTimeout(() => { setFortuneState("colors");}, 1200);
-    setTimeout(() => { setFortuneState("even");}, 1400);
-
-  }
-
+function buttonPress(){
+  setView("Button");
+  setPageVisibility(false);
+}
   return (
-    <>
-      {fortuneState === "colors" && <FortuneColors />}
-      {fortuneState === "even" && <FortuneEven />}
-      {fortuneState === "odd" && <FortuneOdd />}
-
-      <form className="fortuneSelect">
-        <input></input>
-        <button onClick={red}>go!</button>
-      </form>
+    <> 
+    {pageVisibility &&
+<button className='pokeMe' onClick={buttonPress}>Poke Me ;)</button>}
+{pageVisibility &&
+  <div className='gallery'>
+  <img className='image' src={lostPokers} alt="Lost Pokers" />
+  <img className='image' src={masterPoker} alt="Master Pokers" />
+  <img className='image' src={pokeMuppets} alt="Poke Muppets" />
+  <img className='image' src={pokeMuppetsTwo} alt="Poke Muppets" />
+  <img className='image' src={pokeMuscle} alt="Poke Muscle" />
+  <img className='image' src={pokeOneH} alt="Poke 100" />
+  <img className='image' src={pokeTwoTt} alt="Poke 2000" />
+  <img className='image' src={pokeOneT} alt="Poke 100" />
+  <img className='image' src={pokeTwoT} alt="Poke 2000" />
+  </div>}
+  <div>{view === "Button" && <Button />}
+  {view === "App" && <App />}
+  </div>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/Button.jsx
@@ -0,0 +1,10 @@
+
+
+export default function Button() {
+  return (
+    <> 
+  <div className='title'>Holy Pokes!!!!</div>
+  <div className='subTitle'>LEVEL 3000</div>
+    </>
+  )
+}
